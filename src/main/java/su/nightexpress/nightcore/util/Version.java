7ef1b9c582c90310a207eea32de8e5bc89ca24b1
manrajmaan <manrajmaan08@gmail.com>
2025-12-06 15:54:19 +0800

    BROOO

--- src/main/java/su/nightexpress/nightcore/util/Version.java
+++ src/main/java/su/nightexpress/nightcore/util/Version.java
@@ -86,30 +86,6 @@ public enum Version {
         else core.error("[Core Components] " + component.name() + ": Error");
     }
 
-    public static void printCaution(@NotNull NightCorePlugin plugin) {
-        if (current != UNKNOWN && current.isSupported()) return;
-
-        plugin.warn("=".repeat(35));
-
-        if (current == Version.UNKNOWN) {
-            plugin.warn("WARNING: This plugin is not supposed to run on this server version!");
-            plugin.warn("If server version is newer than " + values()[values().length - 1] + ", then wait for an update please.");
-            plugin.warn("Otherwise this plugin will not work properly or even load.");
-        }
-        else if (current.isDeprecated()) {
-            plugin.warn("WARNING: You're running an outdated server version (" + current.getLocalized() + ")!");
-            plugin.warn("This version will no longer be supported in future relases.");
-            plugin.warn("Please upgrade your server to " + Lists.next(current, (Version::isSupported)).getLocalized() + ".");
-        }
-        else if (current.isDropped()) {
-            plugin.error("ERROR: You're running an unsupported server version (" + current.getLocalized() + ")!");
-            plugin.error("Please upgrade your server to " + Lists.next(current, (Version::isSupported)).getLocalized() + ".");
-        }
-
-        plugin.warn("ABSOLUTELY NO DISCORD SUPPORT WILL BE PROVIDED");
-        plugin.warn("=".repeat(35));
-    }
-
     @NotNull
     public static Version getCurrent() {
         return current;
